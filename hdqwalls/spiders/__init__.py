8472835ef49d934d9fde9097d31b2b3faa73d560
serephus <i@sereph.us>
2026-05-04 10:29:26 +0800

    <init> hdqwalls

---
+++ hdqwalls/spiders/__init__.py
@@ -0,0 +1,39 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+from typing import Any, Generator, Optional
+
+from scrapy.http.request import Request
+from scrapy.http.response import Response
+from scrapy.spiders import Spider
+
+from ..items import HdqwallsItem
+
+
+class Hdqwalls(Spider):
+    name = "hdqwalls"
+    allowed_domains = ["hdqwalls.com"]
+    start_urls = ["https://hdqwalls.com/latest-wallpapers/page/1"]
+
+    def parse(
+        self, response: Response
+    ) -> Generator[Request | HdqwallsItem, Any, None]:
+        # let scrapy deduplicate page urls for us
+        # pages: Iterable[str] = response.css("ul.pagination li a::attr(href)")
+        for page in response.css("ul.pagination li a::attr(href)"):
+            url: Optional[str] = page.get()
+            if url is None:
+                continue
+            yield response.follow(url, callback=self.parse)
+
+        image_urls = [
+            # the origin url is just thumb url without the thumb part
+            url.replace("thumb/", "")
+            for wallpaper in response.css(
+                "div.wallpapers_container div.wall-resp"
+            )
+            if (url := wallpaper.css("img::attr(src)").get()) is not None
+        ]
+        yield HdqwallsItem(image_urls=image_urls)
